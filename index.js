c134bd982d1263770c66f5af07c45756a93664b4
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-06 15:18:59 +0300

    add functions

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate") {
+  return `This Saturday, I want to ${fun}!`;
+}
+
+const mondayWork = function (work = "go to the office") {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(flair="*") {
+    return function (arg = "special") {
+        return `You are ${flair}${arg}${flair}!`;
+    }
+}
